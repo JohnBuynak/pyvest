b6b8d3bf3caf60eba9e4073c640704a7e9dca060
JohnBuynak <41218781+JohnBuynak@users.noreply.github.com>
2020-11-24 20:12:45 -0500

    Update options.py

--- pyvest/options.py
+++ pyvest/options.py
@@ -271,8 +271,7 @@ def optionvega(spot, strike, volatility, ttm, risk_free = 0.02, dividend_yield =
     return (1/100)*(s*np.exp(-d*t)*np.sqrt(t)*(1/np.sqrt(2*np.pi))*np.exp((-d1**2)/2))
 
 
-
-def impliedvol(option = 'call', price = 2.92, spot = 100, strike = 100, ttm = 45/365, risk_free = 0, dividend_yield = 0):
+def impliedvol(premium, spot, strike, ttm, risk_free = 0, dividend_yield = 0, calls = True, error = 0.01, vol = None):
     
     """
     Calculates the implied volatility of a European-style option using the Black-Scholes option pricing
@@ -287,55 +286,81 @@ def impliedvol(option = 'call', price = 2.92, spot = 100, strike = 100, ttm = 45
     risk_free =  Annual continuously-compounded risk-free rate
     dividend_yield = Annual continuously-compounded dividend yield
     """
+    if not vol:
+        testvols = np.array([0.001, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.15, 0.25, 0.50, 0.75])
+        
+    else:
+        testvols = np.array([0.01, vol*75, vol*0.90, vol, vol*1.10, vol * 1.25])
     
-    vol = 1e-10
+    if calls:
+        
+        testvals = np.array([(premium - callpremium(spot = spot, strike = strike, volatility = x, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield))/premium for x in testvols])
     
-    if option == 'call':
-    
-        while vol < 100:
-            
-            diff1 = abs(price - callpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm))
-    
-            vol = vol + 0.05
-            
-            diff2 = abs(price - callpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm))
-    
-            if diff2 > diff1:
-                
-                vol = vol - 0.05
-                
-                difference = abs(price - callpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm))
-                
-                while difference >= 0.005:
-                    
-                    difference = price - callpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm)
-                    
-                    vol = vol + 0.0001
-                
-                return vol
     else:
         
-        while vol < 100:
-            
-            diff1 = abs(price - putpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm))
+        testvals = np.array([(premium - putpremium(spot = spot, strike = strike, volatility = x, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield))/premium for x in testvols])
+
     
-            vol = vol + 0.05
+    npmin = np.min(abs(testvals))
+
+    npargwhere = np.argmin(abs(testvals))
+    #return npargwhere, len(testvals)
+    if npargwhere < len(testvals)-1:
+        
+        if npmin <= 0:
+        
+            point1x = testvols[npargwhere - 1]
+            point1y = testvals[npargwhere - 1]
+            point2x = testvols[npargwhere]
+            point2y = testvals[npargwhere]
+        
+        else:
             
-            diff2 = abs(price - putpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm))
+            point1x = testvols[npargwhere]
+            point1y = testvals[npargwhere]
+            point2x = testvols[npargwhere + 1]
+            point2y = testvals[npargwhere + 1]
+    else:
+        
+        if calls:
+            return impliedvol(calls = True,premium = premium, spot = spot, strike = strike, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield, vol = testvols[-1])
+        else:
+            return impliedvol(calls = False,premium = premium, spot = spot, strike = strike, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield, vol = testvols[-1])
+            
+    slope = (point2y-point1y)/(point2x-point1x)
     
-            if diff2 > diff1:
-                
-                vol = vol - 0.05
-                
-                difference = abs(price - putpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm))
-                
-                while difference >= 0.005:
-                    
-                    difference = price - putpremium(spot = spot, strike = strike, volatility = vol, ttm = ttm)
-                    
-                    vol = vol + 0.0001
-                
-                return vol
+    testiv = point1x - (point1y/slope)
+    
+    #print(testiv)
+    if calls:
+        
+        if np.isnan(callpremium(spot = spot, strike = strike, volatility = testiv, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield)):
+            return np.nan
+        
+    else:
+        
+        if np.isnan(putpremium(spot = spot, strike = strike, volatility = testiv, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield)):
+            return np.nan
+    #return (premium - opt.callpremium(spot = spot, strike = strike, volatility = testiv, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield))/premium
+    
+    if calls:
+    
+        if (premium - callpremium(spot = spot, strike = strike, volatility = testiv, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield)) < error:
+    
+            return testiv
+    
+        else:
+    
+            return impliedvol(calls = True, premium = premium, spot = spot, strike = strike, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield, vol = testiv)
+    else:
+        
+        if (premium - putpremium(spot = spot, strike = strike, volatility = testiv, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield)) < error:
+    
+            return testiv
+    
+        else:
+    
+            return impliedvol(calls = False, premium = premium, spot = spot, strike = strike, ttm = ttm, risk_free = risk_free, dividend_yield = dividend_yield, vol = testiv)
 
 
 
